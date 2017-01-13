1fe71ec305a7400c5ab9b9ea0fe352647690db84
Nickolai Novik <nickolainovik@gmail.com>
2017-01-13 23:46:22 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,38 @@
-import codecs
-from setuptools import setup, find_packages
 import os
 import re
+import sys
+from setuptools import setup, find_packages
 
 
-with codecs.open(os.path.join(os.path.abspath(os.path.dirname(
-        __file__)), 'aiohttp_sse', '__init__.py'), 'r', 'latin1') as fp:
-    try:
-        version = re.findall(r"^__version__ = '([^']+)'$", fp.read(), re.M)[0]
-    except IndexError:
-        raise RuntimeError('Unable to determine version.')
+PY_VER = sys.version_info
+
+if not PY_VER >= (3, 4):
+    raise RuntimeError("aiohttp-sse doesn't support Python earlier than 3.4")
 
 
 def read(f):
     return open(os.path.join(os.path.dirname(__file__), f)).read().strip()
 
+
+def read_version():
+    regexp = re.compile(r"^__version__\W*=\W*'([\d.abrc]+)'")
+    init_py = os.path.join(os.path.dirname(__file__),
+                           'aiohttp_admin', '__init__.py')
+    with open(init_py) as f:
+        for line in f:
+            match = regexp.match(line)
+            if match is not None:
+                return match.group(1)
+        else:
+            msg = 'Cannot find version in aiohttp_admin/__init__.py'
+            raise RuntimeError(msg)
+
+
 install_requires = ['aiohttp>=0.14']
-tests_require = install_requires + ['nose']
 
 
-setup(name='aiohttp_sse',
-      version=version,
+setup(name='aiohttp-sse',
+      version=read_version(),
       description=("Server-sent events  support for aiohttp."),
       lond_description='\n\n'.join((read('README.rst'), read('CHANGES.txt'))),
       classifiers=[
@@ -28,15 +40,14 @@ setup(name='aiohttp_sse',
           'Intended Audience :: Developers',
           'Programming Language :: Python',
           'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 3.3',
           'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
           'Topic :: Internet :: WWW/HTTP'],
       author="Nikolay Novik",
       author_email="nickolainovik@gmail.com",
-      url='https://github.com/jettify/aiohttp_sse/',
+      url='https://github.com/aio-libs/aiohttp_sse/',
       license='Apache 2',
       packages=find_packages(),
       install_requires=install_requires,
-      tests_require=tests_require,
-      test_suite='nose.collector',
       include_package_data=True)
