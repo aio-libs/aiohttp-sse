f43454563548256f515f732bc67e702dbe39f4f2
Nicolas Delaby <nicolas@noa.one>
2017-01-15 15:33:19 +0100

    rely on pytest-asyncio plugin
    And cleanup pending tasks before exiting tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,3 @@
-import asyncio
-import gc
-import socket
-
 import pytest
 
 
@@ -9,62 +5,3 @@ import pytest
                 ids=['debug:true', 'debug:false'])
 def debug(request):
     return request.param
-
-
-@pytest.yield_fixture
-def loop(request, debug):
-    old_loop = asyncio.get_event_loop()
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(None)
-    loop.set_debug(debug)
-    yield loop
-
-    if not loop._closed:
-        loop.call_soon(loop.stop)
-        loop.run_forever()
-        loop.close()
-        gc.collect()
-    asyncio.set_event_loop(old_loop)
-
-
-@pytest.mark.tryfirst
-def pytest_pycollect_makeitem(collector, name, obj):
-    if collector.funcnamefilter(name):
-        item = pytest.Function(name, parent=collector)
-        if 'run_loop' in item.keywords:
-            return list(collector._genfunctions(name, obj))
-
-
-@pytest.mark.tryfirst
-def pytest_pyfunc_call(pyfuncitem):
-    """
-    Run asyncio marked test functions in an event loop instead of a normal
-    function call.
-    """
-    if 'run_loop' in pyfuncitem.keywords:
-        funcargs = pyfuncitem.funcargs
-        loop = funcargs['loop']
-        testargs = {arg: funcargs[arg]
-                    for arg in pyfuncitem._fixtureinfo.argnames}
-
-        if not asyncio.iscoroutinefunction(pyfuncitem.obj):
-            func = asyncio.coroutine(pyfuncitem.obj)
-        else:
-            func = pyfuncitem.obj
-        loop.run_until_complete(func(**testargs))
-        return True
-
-
-def pytest_runtest_setup(item):
-    if 'run_loop' in item.keywords and 'loop' not in item.fixturenames:
-        # inject an event loop fixture for all async tests
-        item.fixturenames.append('loop')
-
-
-@pytest.fixture(scope='session')
-def unused_port():
-    def f():
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.bind(('127.0.0.1', 0))
-            return s.getsockname()[1]
-    return f
