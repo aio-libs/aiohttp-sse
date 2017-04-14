0ad4d1fece66e2c8f8f94105562af64fb56a284b
Nicolas Delaby <ticosax@free.fr>
2017-04-14 16:35:24 +0200

    Back to development

--- aiohttp_sse/__init__.py
+++ aiohttp_sse/__init__.py
@@ -5,7 +5,7 @@ import io
 from aiohttp.web import StreamResponse
 from aiohttp.web import HTTPMethodNotAllowed
 
-__version__ = '1.0.0'
+__version__ = '1.0.1-dev'
 __all__ = ['EventSourceResponse', 'sse_response']
 
 
