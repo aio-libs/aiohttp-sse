a1792deb145e41187796f08d77aa3b0d22b602fe
Sam Bull <git@sambull.org>
2024-01-26 22:27:43 +0000

    Update tests/conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,12 +9,6 @@ def debug(request):
     return request.param
 
 
-@pytest.fixture(autouse=True)
-def loop(event_loop, debug):
-    event_loop.set_debug(debug)
-    return event_loop
-
-
 @pytest.fixture
 async def session():
     async with aiohttp.ClientSession() as session:
